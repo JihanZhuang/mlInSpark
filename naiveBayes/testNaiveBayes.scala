5122cae52e6bbdf23e168eeb8755c17f75482fc1
JihanZhuang <zhuanghan1103@163.com>
2016-11-02 13:04:54 +0800

    map to operate the value after select from dataset

--- naiveBayes/testNaiveBayes.scala
+++ naiveBayes/testNaiveBayes.scala
@@ -12,29 +12,37 @@ import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions._
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator 
- 
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import scala.reflect.io.{File,Path} 
+//import org.elasticsearch.spark.sql.EsSparkSQL 
+
 object TestNaiveBayes {
   
-  case class RawDataRecord(category: String, text: String)
+  case class RawDataRecord(category: String,siteId:String,page:String, text: String)
   
   def main(args : Array[String]) {
-    val conf = new SparkConf()
+    val conf = new SparkConf().set("es.index.auto.create","true").set("es.nodes","10.49.89.99:9200")
     val sc = new SparkContext(conf)
     
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     import sqlContext.implicits._
     
-    var srcRDD = sc.textFile("/home/jihanzhuang/bigData/testNaiveBayes/sitePageData.data").map { 
+    var trainRDD = sc.textFile(args(0)).map { 
       x => 
         var data = x.split(",")
-        RawDataRecord(data(0),data(1))
+        RawDataRecord(data(0),data(1),data(2),data(3))
+    }
+
+	var predictRDD = sc.textFile(args(1)).map { 
+      x =>  
+        var data = x.split(",")
+        RawDataRecord(data(0),data(1),data(2),data(3))
     }
     
     //70%作为训练数据，30%作为测试数据
-    val splits = srcRDD.randomSplit(Array(0.5, 0.5))
-    var trainingDF = splits(0).toDF()
-    var testDF = splits(1).toDF()
+    //val splits = srcRDD.randomSplit(Array(0.8, 0.2))
+    var trainingDF = trainRDD.toDF()//splits(0).toDF()
+    var testDF = predictRDD.toDF()//splits(1).toDF()
     
     //将词语转换成数组
     var tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words")
@@ -46,7 +54,7 @@ object TestNaiveBayes {
     var hashingTF = new HashingTF().setInputCol("words").setOutputCol("rawFeatures")
     var featurizedData = hashingTF.transform(wordsData)
     println("output2：")
-    featurizedData.select($"category", $"words", $"rawFeatures").take(1)
+    featurizedData.select($"category", $"words", $"rawFeatures").take(1).foreach(println)
     
     
     //计算每个词的TF-IDF
@@ -54,13 +62,15 @@ object TestNaiveBayes {
     var idfModel = idf.fit(featurizedData)
     var rescaledData = idfModel.transform(featurizedData)
     println("output3：")
+    rescaledData.printSchema
     rescaledData.select($"category", $"features").take(2).foreach(println)
     
 	val toDouble = udf[Double, String]( _.toDouble)
+	val toString = udf((arrayCol: Seq[String]) => arrayCol.mkString(" "))
 	//转换成Bayes的输入格式
     var trainDataRdd = rescaledData.withColumn("label", toDouble(rescaledData("category"))).select($"label",$"features")
     println("output4：")
-trainDataRdd.printSchema();
+trainDataRdd.printSchema()
 println(trainDataRdd.getClass())
 
  val tnaiveBayes = new NaiveBayes().setLabelCol("label").setFeaturesCol("features")
@@ -71,12 +81,20 @@ println(trainDataRdd.getClass())
     var testwordsData = tokenizer.transform(testDF)
     var testfeaturizedData = hashingTF.transform(testwordsData)
     var testrescaledData = idfModel.transform(testfeaturizedData)
-    var testDataRdd = testrescaledData.withColumn("label", toDouble(testrescaledData("category"))).select($"label",$"features")
+    var testDataRdd = testrescaledData.withColumn("label", toDouble(testrescaledData("category")))//.select($"label",$"features")
     
     //对测试数据集使用训练模型进行分类预测
   val predictions = model.transform(testDataRdd)
-    predictions.show(3000)  
-    //统计分类准确率
+predictions.printSchema
+predictions.where("label!=prediction").take(1).foreach(println)  
+val file_path="/home/builder/spark/test.data"
+    val path:Path = Path(file_path)
+    if(path.exists){
+      path.deleteRecursively()
+    } 
+val saveData=predictions.where("label!=prediction").select("label","prediction","siteId","page","probability").map{case Row(label:Double,prediction:Double,siteId:String,page:String,probability:Vector)=>(label,prediction,siteId,page,probability(1))}.rdd.coalesce(1,true).saveAsTextFile(file_path)
+//    EsSparkSQL.saveToEs(saveData,"test/test") 
+//统计分类准确率
     println("output5：")
 val evaluator = new MulticlassClassificationEvaluator()
       .setLabelCol("label")
